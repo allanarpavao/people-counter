376d5c31031be222e7852387f9a2628719dce0e8
allanapavao <allanarodrigues.p@gmail.com>
2026-05-14 13:03:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// initialize the count as 0
+let count = 0
+let countEl = document.getElementById("count-el")
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+function increment() {
+    count += 1
+    document.getElementById("count-el").innerText = count
+}
