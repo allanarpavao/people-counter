7ba1541cce4c9027fa59937e9ec20105676fda24
allanapavao <allanarodrigues.p@gmail.com>
2026-05-15 15:07:10 -0300

    style: add commas

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // initialize the count as 0
-let count = 0
-let countEl = document.getElementById("count-el")
+let count = 0;
+let countEl = document.getElementById("count-el");
 
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
 function increment() {
-    count += 1
-    countEl.innerText = count
+    count += 1;
+    countEl.innerText = count;
 }
